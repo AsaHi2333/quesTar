287dfaee3f84417d7e043227385892dfead47113
我和你爆了 <39001701+169532319@users.noreply.github.com>
2024-05-06 00:08:36 +0800

    删除

--- questionnaire/src/main/java/com/questionnaire/controller/UserController.java
+++
@@ -1,7 +0,0 @@
-package com.questionnaire.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UserController {
-}
--- questionnaire/src/main/java/com/questionnaire/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.questionnaire.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper {
-}
