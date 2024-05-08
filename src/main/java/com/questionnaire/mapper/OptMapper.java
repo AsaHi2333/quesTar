c81ad8776549d00c9e23b17fa4fd7f0ea9eb47dd
我和你爆了 <39001701+169532319@users.noreply.github.com>
2024-05-08 10:58:16 +0800

    完成选项操作

--- src/main/java/com/questionnaire/mapper/OptMapper.java
+++ src/main/java/com/questionnaire/mapper/OptMapper.java
@@ -10,11 +10,9 @@ import java.util.List;
 @Mapper
 public interface OptMapper {
     //根据问题id查询所有选项
-    @Select("SELECT * from opt where question_id=#{questionId}")
     List<Opt> listOptByQuestionId(Integer questionId);
 
     //根据选项id删除选项
-    @Delete("DELETE from opt where id=#{id}")
     void deleteOptById(Integer id);
 
     //新建选项
