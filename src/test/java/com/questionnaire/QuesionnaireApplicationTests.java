848d66c4a926de8473bc2de0a30d99f43205125d
我和你爆了 <39001701+169532319@users.noreply.github.com>
2024-06-01 17:52:48 +0800

    将手机号改为邮箱 增加验证码功能

--- src/test/java/com/questionnaire/QuesionnaireApplicationTests.java
+++ src/test/java/com/questionnaire/QuesionnaireApplicationTests.java
@@ -1,13 +1,15 @@
 package com.questionnaire;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class QuesionnaireApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
+
 
 }
